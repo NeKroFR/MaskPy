3e59cb19110a3245c0dd604d5bfe28048bbba89e
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-03-26 16:59:23 +0200

    Update Maskpy.py

--- src/Maskpy.py
+++ src/Maskpy.py
@@ -42,8 +42,17 @@ def randkey(swap):
         key =''.join(random.choices(string.ascii_lowercase, k=random.randint(6,10)))
     return key
 
+def get_indent(line):
+    indent = ''
+    for chr in line:
+        if chr == " ":
+            indent += chr
+        else:
+            break
+    return indent
+
+
 def obfuscate(code):
-    #prendre en compte l'indentation
     swap = {}
     obfuscated_code = []
     for l in code:
@@ -55,9 +64,9 @@ def obfuscate(code):
                 key = randkey(swap)
                 obf_line.append(key)
                 swap[word] = key
-        obfuscated_code.append(' '.join(obf_line))
+        obfuscated_code.append(get_indent(l)+' '.join(obf_line)+"\n")
 
-    # Reverse keys and values in swap
+    # Reverse keys and values ​​in swap
     swap = {v: k for k, v in swap.items()}
     return "import re\nswap ="+str(swap) + '\ncode=  """'+''.join(obfuscated_code)+ '"""'+"\n(lambda: exec(re.compile('|'.join(map(re.escape, swap.keys()))).sub(lambda match: swap[match.group(0)], code)))()"
 
