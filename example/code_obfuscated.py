3489b62b21b1de6ed1e29437fb84db6287c56cb0
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-08-19 11:09:38 +0200

    Update code_obfuscated.py

--- example/code_obfuscated.py
+++ example/code_obfuscated.py
@@ -1,4 +1,4 @@
 import re
-hgmwtabcm=(lambda cipher_dict, key: {k: ''.join([chr((ord(v[i]) - ord(key[i % len(key)])) % 256) for i in range(len(v))]) for k, v in cipher_dict.items()})({'pwznltyyrk': '×ÍÚ', 'abmjad': 'Ô\x90ì\x9a¬', 'bzqexcy': 'Ù×æ', 'rzfqfe': 'Ü', 'sohgdlrdwh': 'ÜÖ', 'kjmdyibnoz': 'åÉâØ×\x9d\x98\x98ì\x91ª¡\x9d', 'viiizbama': 'ãÚÝßæ\x9dÐ\x95', 'jgucggz': 'Ô\x90¥¡\x9b'},'shtqrugltfyxcbmv')
-cfihjj=  """pwznltyyrk abmjad\n    bzqexcy rzfqfe sohgdlrdwh kjmdyibnoz\n        viiizbama\njgucggz\n"""
-(lambda: exec(re.compile('|'.join(map(re.escape, hgmwtabcm.keys()))).sub(lambda match: hgmwtabcm[match.group(0)], cfihjj)))()
+lvqlqu=(lambda jydfivpkn, jbtfkwfur: {k: ''.join([chr((ord(v[i]) - ord(jbtfkwfur[i % len(jbtfkwfur)])) % 256) for i in range(len(v))]) for k, v in jydfivpkn.items()})({'djrkoqhpx': 'ÜÛÒ', 'oratannnek': 'Ù\x9eä¢´', 'zzuwfu': 'ÞåÞ', 'yanalokq': 'á', 'uswonbrvvs': 'áä', 'qwyntpd': 'ê×Úàß\x9b\xa0¥ê¤\x9e¢®', 'zmtopmo': 'èèÕçî\x9bØ¢', 'qitpaotw': 'Ù\x9e\x9d©£'},'xvlyzsoyrymytzvt')
+higmqcsg=  """djrkoqhpx oratannnek\n    zzuwfu yanalokq uswonbrvvs qwyntpd\n        zmtopmo\nqitpaotw\n"""
+(lambda: exec(re.compile('|'.join(map(re.escape, lvqlqu.keys()))).sub(lambda match: lvqlqu[match.group(0)], higmqcsg)))()
